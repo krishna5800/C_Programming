db173bad3b1eba11a7cf8422c3b25c32958bab43
Krishna Hitnalikar <krishnahitnalikar4@gmail.com>
2026-01-05 23:17:38 +0530

    Assignemnt 50

---
+++ Assignments/Assignment 50/program50_5.c
@@ -0,0 +1,103 @@
+// 5) Display Nodes at Nth Position
+
+// Void Dispay (PNODE Head, int pos);
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if((*first) == NULL)
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        newn->next = (*first);
+        (*first) = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d | ->",first->data);
+        first = first->next;
+    }
+
+    printf("NULL\n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)
+    {
+        iCount++;
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void DisplayNth(PNODE first, int pos)
+{
+    PNODE temp = first;
+    int iCount = 0;
+
+    iCount = 1;
+    while(temp != NULL)
+    {
+        if(iCount == pos)
+        {
+            printf("Data at %dth node is : %d", pos, temp->data);
+        }
+
+        iCount++;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertFirst(&head, 111);
+    InsertFirst(&head, 102);
+    InsertFirst(&head, 101);
+    InsertFirst(&head, 51);
+    InsertFirst(&head, 28);
+    InsertFirst(&head, 21);
+    InsertFirst(&head, 11);
+    InsertFirst(&head, 10);
+
+    Display(head);
+    iRet = Count(head);
+
+    printf("Number of nodes are : %d\n", iRet);
+
+    DisplayNth(head, 5);
+    
+    return 0;
+}
