fe09d121435ab0c6f42a02d151cc531bfb677bd5
Krishna Hitnalikar <krishnahitnalikar4@gmail.com>
2026-02-12 23:44:50 +0530

    Recursion of LL

---
+++ ClassWork/Day9/program735.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data ;
+
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void DisplayI(PNODE first)
+{
+    while(first != NULL)                    // Type 1
+    {
+        printf("| %d | -> ", first->data);
+        first = first->next;
+    }
+
+    printf("NULL \n");
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while(first != NULL)                    // Type 1
+    {
+        iCount++;
+
+        first = first->next;
+    }
+
+    return iCount;
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+
+    if((*first) == NULL)                    // LL is empty
+    {
+        (*first) = newn;
+    }
+    else
+    {
+        temp = (*first);
+            
+        while(temp->next != NULL)       // type 2
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newn; 
+    }
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+
+    InsertLast(&head, 11);
+    InsertLast(&head, 21);
+    InsertLast(&head, 51);
+    InsertLast(&head, 101);
+
+    DisplayI(head);
+
+    return 0;
+}
